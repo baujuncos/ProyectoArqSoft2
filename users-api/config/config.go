0def40e3e7df30369d029bda845ea18cf1d59193
baujuncos <127853844+baujuncos@users.noreply.github.com>
2024-11-10 18:56:34 -0300

    Update config.go

--- users-api/config/config.go
+++ users-api/config/config.go
@@ -3,17 +3,17 @@ package config
 import "time"
 
 const (
-	MySQLHost     = "localhost"
+	MySQLHost     = "MySQL"
 	MySQLPort     = "3306"
 	MySQLDatabase = "users-api"
 	MySQLUsername = "root"
-	MySQLPassword = "root"
+	MySQLPassword = "ladrillo753"
 
-	CacheDuration = 30 * time.Second
+	CacheDuration = 30 * time.Second //tiempo que datos se mantienen en cache 30 segundos
 
-	MemcachedHost = "localhost"
+	MemcachedHost = "memCached"
 	MemcachedPort = "11211"
 
 	JWTKey      = "ThisIsAnExampleJWTKey!"
-	JWTDuration = 24 * time.Hour
+	JWTDuration = 24 * time.Hour //tiempo de validez de token: 24 horas
 )
