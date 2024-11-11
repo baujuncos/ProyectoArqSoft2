8484ab42d12b76e1d6c052babfe002e9de27f811
baujuncos <127853844+baujuncos@users.noreply.github.com>
2024-11-11 18:34:26 -0300

    Update users_dao.go

--- users-api/dao/users_dao.go
+++ users-api/dao/users_dao.go
@@ -1,11 +1,11 @@
 package dao
 
 type Users struct {
-	User_id  int    `gorm:"primaryKey;AUTO_INCREMENT"`
-	Email    string `gorm:"type:varchar(100);not null;unique"`
-	Password string `gorm:"type:varchar(100);not null"`
-	Nombre   string `gorm:"type:varchar(100);not null"`
-	Apellido string `gorm:"type:varchar(100);not null"`
-	Admin    bool   `gorm:"type:boolean;not null"`
+	User_id  int64  `gorm:"primaryKey;AUTO_INCREMENT;column:user_id"`
+	Email    string `gorm:"type:varchar(100);not null;unique" binding:"required"`
+	Password string `gorm:"type:varchar(100);not null" binding:"required"`
+	Nombre   string `gorm:"type:varchar(100);not null" binding:"required"`
+	Apellido string `gorm:"type:varchar(100);not null" binding:"required"`
+	Admin    bool   `gorm:"type:boolean;not null" binding:"required"`
 }
 type Userss []Users
