6210250501fba7bb4092e8fed3f28b99d49b2d9e
belutreachi <belutreachi22@gmail.com>
2024-12-17 16:59:52 -0300

    cambio login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Cookies from 'js-cookie';
 import { Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
-import '../estilos/Login.css'
+import '../estilos/Login.css';
 
 const PasswordInput = ({ password, setPassword }) => {
     const [show, setShow] = React.useState(false);
@@ -15,10 +15,10 @@ const PasswordInput = ({ password, setPassword }) => {
                 placeholder='Ingrese su contraseña...'
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                style={{ border:'2px solid black', fontFamily: 'Spoof Trial, sans-serif'}}
+                style={{ border: '2px solid black', fontFamily: 'Spoof Trial, sans-serif' }}
             />
             <InputRightElement width='4.5rem'>
-                <Button h='1.75rem' size='sm' onClick={handleClick} style={{fontFamily: 'Spoof Trial, sans-serif'}}>
+                <Button h='1.75rem' size='sm' onClick={handleClick} style={{ fontFamily: 'Spoof Trial, sans-serif' }}>
                     {show ? 'Ocultar' : 'Mostrar'}
                 </Button>
             </InputRightElement>
@@ -33,16 +33,16 @@ const Login = ({ onClose }) => {
     Cookies.set('user_id', 0);
 
     const handleSubmit = async (e) => {
-        e.preventDefault(); // Para que no recargue la página
+        e.preventDefault(); // Evita que se recargue la página
         let emptyLogin = false;
 
         if (email === '') {
-            document.getElementById('inputEmailLogin');
+            alert('El campo de correo electrónico es obligatorio.');
             emptyLogin = true;
         }
 
         if (password === '') {
-            document.getElementById('inputPasswordLogin');
+            alert('El campo de contraseña es obligatorio.');
             emptyLogin = true;
         }
 
@@ -53,22 +53,24 @@ const Login = ({ onClose }) => {
                     headers: {
                         'Content-Type': 'application/json',
                     },
-                    body: JSON.stringify({email, password}),
+                    body: JSON.stringify({ email, password }),
                 });
 
                 if (response.ok) {
                     const data = await response.json();
                     console.log('Respuesta del servidor:', data);
-                    if(data.user_id){
+
+                    if (data.user_id) {
                         Cookies.set('user_id', data.user_id, { expires: 1, path: '/' });
                         Cookies.set('email', email, { expires: 1, path: '/' });
                         Cookies.set('token', data.token, { expires: 1, path: '/' });
                         Cookies.set('admin', data.admin ? "1" : "0", { expires: 1, path: '/' });
 
+                        onClose(); // Cierra el popup solo si todo es exitoso
                         window.location.reload();
                     }
                 } else {
-                    alert("Usuario no registrado o Contraseña incorrecta");
+                    alert("Usuario no registrado o contraseña incorrecta.");
                 }
             } catch (error) {
                 Cookies.set('user_id', "-1");
@@ -84,10 +86,10 @@ const Login = ({ onClose }) => {
                 placeholder="Ingrese su correo electrónico..."
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                style={{ border:'2px solid black', fontFamily: 'Spoof Trial, sans-serif'}}
+                style={{ border: '2px solid black', fontFamily: 'Spoof Trial, sans-serif' }}
             />
             <PasswordInput password={password} setPassword={setPassword} />
-            <Button colorScheme="blue" mr={3} type="submit" onClick={onClose}>
+            <Button colorScheme="blue" mr={3} type="submit">
                 Iniciar sesión
             </Button>
         </form>
